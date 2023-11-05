44daee3be5ecd1fcad4e37683659920b4a728cb6
Berindeiee <adrian.berindeie10@e-uvt.ro>
2023-11-05 19:22:51 +0200

    123

--- buildbuddy-client/client/src/App.tsx
+++ buildbuddy-client/client/src/App.tsx
@@ -1,29 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect, useRef } from 'react';
+import { Button } from '@mui/material';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isIncrementing, setIsIncrementing] = useState(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (isIncrementing) {
+      // Creează un interval care incrementează contorul la fiecare 100 ms
+      intervalRef.current = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 100);
+    } else {
+      // Curăță intervalul când nu mai este necesar
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    }
+    // Curăță intervalul când componenta este demontată
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+  }, [isIncrementing]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button
+          variant="contained"
+          color="primary"
+          onMouseDown={() => setIsIncrementing(true)}
+          onMouseUp={() => setIsIncrementing(false)}
+          onMouseLeave={() => setIsIncrementing(false)}
+          onClick={() => setCount((count)=>count+1)}
+        >
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
@@ -32,4 +55,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
